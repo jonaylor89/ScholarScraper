951e7fd0d3f6d2a176ef0da25a894aa6d5433260
John Naylor <jonaylor89@gmail.com>
2019-01-31 18:40:43 -0500

    black formatting

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
 from selenium.webdriver import Firefox
-from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
 
 option = Options()
 option.headless = True
 browser = Firefox(options=option)
-browser.get('http://scholar.google.com')
+browser.get("http://scholar.google.com")
 
-search = browser.find_element_by_name('q')
+search = browser.find_element_by_name("q")
 
 search.send_keys("Alberto Cano")
 search.send_keys(Keys.RETURN)
