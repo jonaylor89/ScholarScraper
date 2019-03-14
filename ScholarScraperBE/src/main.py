0110cb2a191d9cc62e0bc6a746e0326143eab1bc
John Naylor <jonaylor89@gmail.com>
2019-03-13 20:52:38 -0400

    Doc string

--- ScholarScraperBE/src/main.py
+++ ScholarScraperBE/src/main.py
@@ -4,18 +4,24 @@ import json
 import logging
 from .scraper import scraper
 from flask_cors import CORS
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
 def hello():
+    """
+    Hello message mostly used for development error checking
+    """
     return "WELCOME TO SCHOLAR SCRAPER!"
 
 
 @app.route("/name/<name>", methods=["GET"])
 def parse_by_name(name: str):
+    """
+    The main meat and potatoes right now
+    """
 
     json_data = ""
 
@@ -29,3 +35,10 @@ def parse_by_name(name: str):
             json_data = json.loads(f.read())
 
     return json.dumps(json_data[name], indent=2)
+
+@app.route("/secret", methods=["GET", "POST"])
+def secret():
+    """
+    A secret
+    """
+    return redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
