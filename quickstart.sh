6964d6822435f5e44cc193875ea4202908aceec2
John Naylor <jonaylor89@gmail.com>
2019-03-14 21:18:45 -0400

    Beuatify

--- quickstart.sh
+++ quickstart.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # Basic message
-printf "TO EXIT BACK TO THE SHELL, HIT CTRL-C \n\n\n"
+printf "######################################\n"
+printf "TO EXIT BACK TO THE SHELL, HIT CTRL-C \n"
+printf "######################################\n\n\n"
 
 # It's literally one command because docker
 docker-compose up
