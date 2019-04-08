bff64320b0e6e575f619e1e33fc261f0b0e5e2ac
Rasha Saadeh <>
2019-04-08 19:22:14 -0400

    Add/Delete Scholars functionality

--- ScholarScraperFE/src/app/app.component.ts
+++ ScholarScraperFE/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  scholars = [];
+  addScholar(newScholar: string) {
+    if (newScholar) {
+      this.scholars.push(newScholar);
+    }
+  }
+
+    deleteScholar(deleteScholar: string) {
+      console.log(this.scholars)
+      for (var i = 0; i < this.scholars.length; i++) {
+        if(this.scholars[i] === deleteScholar){
+          this.scholars.splice(i,1);
+        }
+      }
+      
+    
+  
+    
+    
+    // document.getElementById('button').onclick = function() {
+    //   // alert("button was clicked");
+    //   var scholarID = document.getElementById('scholarID').innerText;
+    //   alert(scholarID + "Hello");
+    // }​
+  }
   title = 'ScholarScraperFE';
 }
+
+
