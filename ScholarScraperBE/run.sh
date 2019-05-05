30b3aa84231785eb46bbe53197184b583acc80a3
John Naylor <jonaylor89@gmail.com>
2019-05-05 14:59:47 -0400

    Style

--- ScholarScraperBE/run.sh
+++ ScholarScraperBE/run.sh
@@ -4,9 +4,9 @@
 pipenv install
 
 # Reminder
-printf "+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=\n"
-printf "+= MAKE SURE THE ENVIRONMENT VARIABLE DB_PASSWORD IS SET =+\n"
-printf "+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=\n"
+printf "+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+\n"
+printf "+= MAKE SURE THE ENVIRONMENT VARIABLES ARE SET =+\n"
+printf "+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+\n"
 
 # Serve API
 pipenv run gunicorn -b0.0.0.0:8000 src.main:app
