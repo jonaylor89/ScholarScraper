2d7dbd8d2bd6892f8174c1b5f244d8875b76ee75
John Naylor <jonaylor89@gmail.com>
2019-05-05 14:52:19 -0400

    Cleaning things up

--- ScholarScraperBE/src/main.py
+++ ScholarScraperBE/src/main.py
@@ -89,28 +89,6 @@ def get_publications():
     return jsonify(publications.data)
 
 
-@app.route("/publication", methods=["POST"])
-def publication():
-    """
-    Parse people
-    """
-    # mount scholar object
-    posted_publication = PublicationSchema().load(request.get_json())
-
-    publication = Publication(**posted_publication.data, created_by="HTTP post request")
-
-    # persist scholar
-    session = Session()
-    session.add(publication)
-    session.commit()
-
-    # return created scholar
-    new_publication = PublicationSchema().dump(publication.data)
-    session.close()
-
-    return jsonify(new_publication), 201
-
-
 @app.route("/publication-author")
 def get_publication_author():
 
@@ -126,30 +104,6 @@ def get_publication_author():
     session.close()
 
     return jsonify(publication_author.data)
-
-
-@app.route("/publication_author", methods=["POST"])
-def publication_author():
-    """
-    Parse people
-    """
-    # mount scholar object
-    posted_publication_author = PublicationAuthorSchema().load(request.get_json())
-
-    publication_author = PublicationAuthor(
-        **posted_publication_author.data, created_by="HTTP post request"
-    )
-
-    # persist scholar
-    session = Session()
-    session.add(publication_author)
-    session.commit()
-
-    # return created scholar
-    new_publication_author = PublicationAuthorSchema().dump(publication_author.data)
-    session.close()
-
-    return jsonify(new_publication_author), 201
 
 
 @app.route("/publication-cites")
@@ -169,30 +123,6 @@ def get_publication_cites():
     return jsonify(publication_cites.data)
 
 
-@app.route("/publication-cites", methods=["POST"])
-def publication_cites():
-    """
-    Parse people
-    """
-    # mount scholar object
-    posted_publication_cites = PublicationCitesSchema().load(request.get_json())
-
-    publication_cites = PublicationCites(
-        **posted_publication_cites.data, created_by="HTTP post request"
-    )
-
-    # persist scholar
-    session = Session()
-    session.add(publication_cites)
-    session.commit()
-
-    # return created scholar
-    new_publication_cites = PublicationCitesSchema().dump(publication_cites.data)
-    session.close()
-
-    return jsonify(new_publication_cites), 201
-
-
 @app.route("/total-citation")
 def get_total_citations():
 
@@ -210,30 +140,6 @@ def get_total_citations():
     return jsonify(total_citations.data)
 
 
-@app.route("/total-citations", methods=["POST"])
-def total_citations():
-    """
-    Parse people
-    """
-    # mount scholar object
-    posted_total_citations = TotalCitationsSchema().load(request.get_json())
-
-    total_citations = TotalCitations(
-        **posted_total_citations.data, created_by="HTTP post request"
-    )
-
-    # persist scholar
-    session = Session()
-    session.add(total_citations)
-    session.commit()
-
-    # return created scholar
-    new_total_citations = TotalCitationsSchema().dump(total_citations.data)
-    session.close()
-
-    return jsonify(new_total_citations), 201
-
-
 @app.route("/secret")
 def secret():
     """
@@ -249,60 +155,3 @@ def not_found(error=None):
     resp.status_code = 404
 
     return resp
-
-
-def update_scholar(data: Dict):
-    """
-    Upload scholar to db
-    """
-    session = Session()
-
-    for name, info in data.items():
-        s = session.query(Scholar).get(info["id"])
-        s.full_name = name
-        print(name)
-        session.commit()
-
-    session.close()
-
-
-def upload_publication(data: Dict):
-    """
-    Upload publication to db
-    """
-    session = Session()
-
-    for name, info in data.items():
-        for title, article_info in info["articles"].items():
-            pub = Publication(
-                article_info["id"], title, article_info["publication_date"], "json file"
-            )
-
-            new_pub = PublicationSchema().dump(pub).data
-
-            print(json.dumps(new_pub, indent=2))
-
-            session.add(pub)
-
-            session.commit()
-
-    session.close()
-
-
-def upload_total_citations(data: Dict) -> None:
-    """
-    Upload total citations to db
-    """
-    session = Session()
-
-    for name, info in data.items():
-        total_cites = TotalCitations(info["id"], info["citation_count"], "json file")
-
-        new_total = TotalCitationsSchema().dump(total_cites).data
-        print(json.dumps(new_total, indent=2))
-
-        session.add(total_cites)
-
-        session.commit()
-
-    session.close()
